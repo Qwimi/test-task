040f23b6859302cab406317ed2ca7cf8953bedc9
qwimi <nastasuntw@gmail.com>
2026-01-27 00:05:26 +0400

    feat: add icon component

---
+++ src/components/svg-icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <component v-if="icon" :is="icon" v-bind="$attrs" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{ name: string }>()
+
+const icons = import.meta.glob('../assets/icons/*.svg', { eager: true })
+
+const icon = computed(() => {
+  const key = `../assets/icons/${props.name}.svg`
+  return icons[key] ? (icons[key] as any).default : null
+})
+</script>
